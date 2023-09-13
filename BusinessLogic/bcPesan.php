65870f2907966ad2ae4b1fb5ebf86a4e49b2b355
progxml <progamerxml@gmail.com>
2023-09-13 15:28:59 +0700

    hari ni testing doang

--- BusinessLogic/bcPesan.php
+++ BusinessLogic/bcPesan.php
@@ -1,14 +1,18 @@
 <?php
 
 require_once __DIR__ . "/../config/koneksi.php";
+require_once __DIR__ . "/../config/config.php";
 require_once __DIR__ . "/../Model/DataBc.php";
 
 
 $today = date("d-m-Y");
 $dataBc = getDataBcbyDate($today);
+// var_dump($dataBc);
+
 // $data_pesan = [];
 // $data_group = [];
 $g = 1;
+$data = [];
 while ( $hasil = mysqli_fetch_object($dataBc) ) {           // dapatkan data bc
 
     $dataPesan = getAllPesanId($hasil->id_pesan);           // dapatkan data pesan
@@ -19,11 +23,24 @@ while ( $hasil = mysqli_fetch_object($dataBc) ) {           // dapatkan data bc
     // array_push($data_group, $dataGroup);
     foreach ($dataGroup as $group) {
         $idgroup = getAllGroupId($group);
+        $data = array_push(["pesan" => $dataPesan->isi_pesan, "id_group" => $idgroup->id_group]);
+        // $bcPesan = bcPesan($data);
+        // var_dump($bcPesan);
     }
 }
 
-function bcPesan($today) 
-{
-    $token = "6556939785:AAEc1b9Nk4BR1Pk48heWikKzhqBfUNHH1bM";
-    // $text = "$hasil->"
-}
+var_dump($dataPesan->isi_pesan);
+// function bcPesan($data) 
+// {
+//     global $token;
+//     // $token = "6556939785:AAEc1b9Nk4BR1Pk48heWikKzhqBfUNHH1bM";
+//     $data = [
+//         "text" =>"$data[pesan]",
+//         "chat_id" => "$data[id_group]"  //contoh bot, group id -1001177007534
+//     ];
+     
+//     file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
+//     // $text = "$hasil->"
+// }
+
+// bcPesan($data);
