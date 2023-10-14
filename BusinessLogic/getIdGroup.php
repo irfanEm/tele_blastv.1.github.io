852416a2f346300ea00beaf5005a73a03f56b4e9
irfanEm <progamerxml@gmail.com>
2023-10-14 10:59:28 +0700

    comment code getIdGroup.php karena mengganggu

--- BusinessLogic/getIdGroup.php
+++ BusinessLogic/getIdGroup.php
@@ -2,49 +2,49 @@
 
 require_once __DIR__ . "/../config/config.php";
 
-function getIdGroup()
-{
-  global $token;
+// function getIdGroup()
+// {
+//   global $token;
 
-  $url = "https://api.telegram.org/bot$token/getUpdates"; // Ganti URL dengan URL yang sesuai
-  $response = file_get_contents($url);
-  $data = json_decode($response, true);
-  return $data;
+//   $url = "https://api.telegram.org/bot$token/getUpdates"; // Ganti URL dengan URL yang sesuai
+//   $response = file_get_contents($url);
+//   $data = json_decode($response, true);
+//   return $data;
 
-}
+// }
 
-$array = getIdGroup();
-$key = "id";
-$value = "";
-function searchNestedArray($array, $key) {
-    $result = array();
-    array_walk_recursive($array, function($value, $index) use ($key, &$result) {
-        if ($index == $key) {
-            array_push($result, $value);
-        }
-    });
-    return $result;
-}
+// $array = getIdGroup();
+// $key = "id";
+// $value = "";
+// function searchNestedArray($array, $key) {
+//     $result = array();
+//     array_walk_recursive($array, function($value, $index) use ($key, &$result) {
+//         if ($index == $key) {
+//             array_push($result, $value);
+//         }
+//     });
+//     return $result;
+// }
 
-function searchArray($array, $key, $value)
-{
-    $results = array();
+// function searchArray($array, $key, $value)
+// {
+//     $results = array();
 
-    if (is_array($array))
-    {
-        if (isset($array[$key]) && $array[$key] == $value)
-        {
-            $results[] = $array;
-        }
+//     if (is_array($array))
+//     {
+//         if (isset($array[$key]) && $array[$key] == $value)
+//         {
+//             $results[] = $array;
+//         }
 
-        foreach ($array as $subarray)
-        {
-            $results = array_merge($results, searchArray($subarray, $key, $value));
-        }
-    }
+//         foreach ($array as $subarray)
+//         {
+//             $results = array_merge($results, searchArray($subarray, $key, $value));
+//         }
+//     }
 
-    return $results;
-}
+//     return $results;
+// }
 
 
 // $hasil = getIdGroup();
