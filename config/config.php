f42c31da91fd3261114e17722cf6aca5209cf5b4
progamerxml <progamerxml@outlook.com>
2023-12-28 13:41:03 +0700

    ganti token dan username bot

--- config/config.php
+++ config/config.php
@@ -3,4 +3,4 @@
 $base_url = "http://192.168.1.64/tele_blastv.1.github.io/";
 $timezone = date_default_timezone_set("Asia/Jakarta");
 // $token = "6556939785:AAEc1b9Nk4BR1Pk48heWikKzhqBfUNHH1bM"; // token bot testing
-$token = "6598949420:AAHQ6JDapP7ujwdcXIglHBlslNuYCOUZ9wM"; // token bot xmltronik
+$token = "6948504996:AAF8j-C10GH4ah6Wj6WrspTpMxuofClEfK0"; // token bot xmltronik
